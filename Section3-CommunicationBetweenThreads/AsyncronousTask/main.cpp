310537aeab4735a1514eefde36cfb15a318c034c
wgaonar <wgaonar@gmail.com>
2022-10-29 07:05:27 -0500

    Asynchronous tasks added

--- Section3-CommunicationBetweenThreads/AsyncronousTask/main.cpp
+++ Section3-CommunicationBetweenThreads/AsyncronousTask/main.cpp
@@ -4,35 +4,39 @@
 
 void printing()
 {
-	std::cout << "printing runs on-" << std::this_thread::get_id() << std::endl;
+	std::cout << "printing runs on id: [" << std::this_thread::get_id() << "]\n";
 }
 
 int addition(int x, int y)
 {
-	std::cout << "addition runs on-" << std::this_thread::get_id() << std::endl;
+	std::cout << "addition runs on id: [" << std::this_thread::get_id() << "]\n";
 	return x + y;
 }
 
-int substract(int x, int y)
+int subtraction(int x, int y)
 {
-	std::cout << "substract runs on-" << std::this_thread::get_id() << std::endl;
+	std::cout << "subtraction runs on id: [" << std::this_thread::get_id() << "]\n";
 	return x - y;
 }
 
 int main()
 {
-	std::cout << "main thread id -" << std::this_thread::get_id() << std::endl;
+	std::cout << "main thread runs on this id: [" << std::this_thread::get_id() << "]\n";
 
 	int x = 100;
 	int y = 50;
 
+  // async option runs on a separate thread
 	std::future<void> f1 = std::async(std::launch::async, printing);
+
+  // deferred option runs on the same creator thread
 	std::future<int> f2 = std::async(std::launch::deferred, addition, x, y);
+
+  // deferred | async multiple option lets the choice to the program
 	std::future<int> f3 = std::async(std::launch::deferred | std::launch::async,
-		substract, x, y);
+		                    subtraction, x, y);
 
 	f1.get();
-	std::cout << "value received using f2 future -" << f2.get() << std::endl;
-	std::cout << "value received using f2 future -" << f3.get() << std::endl;
-
+	std::cout << "Value received using f2 (addition) future: " << f2.get() << std::endl;
+	std::cout << "Value received using f3 (subtraction) future: " << f3.get() << std::endl;
 }
